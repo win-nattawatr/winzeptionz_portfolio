a0dfab399ac523ad892af08bdee3737e43b7d29a
winbiosuthi <100664912+nattawatR@users.noreply.github.com>
2023-03-01 15:06:31 +0700

    commit: firebase initial

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,6 +3,7 @@ export const environment = {
     projectId: 'winzeptionportfolio',
     appId: '1:18801088931:web:a7a0beca3562e531c2a216',
     storageBucket: 'winzeptionportfolio.appspot.com',
+    locationId: 'asia-southeast2',
     apiKey: 'AIzaSyDdBvbIIikzazwnrvwE1TDCcTDCh_OZA4k',
     authDomain: 'winzeptionportfolio.firebaseapp.com',
     messagingSenderId: '18801088931',
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,12 +7,13 @@ export const environment = {
     projectId: 'winzeptionportfolio',
     appId: '1:18801088931:web:a7a0beca3562e531c2a216',
     storageBucket: 'winzeptionportfolio.appspot.com',
+    locationId: 'asia-southeast2',
     apiKey: 'AIzaSyDdBvbIIikzazwnrvwE1TDCcTDCh_OZA4k',
     authDomain: 'winzeptionportfolio.firebaseapp.com',
     messagingSenderId: '18801088931',
     measurementId: 'G-FGF3R9D2YM',
   },
-  production: false
+  production: false,
 };
 
 /*
